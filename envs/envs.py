1e20c874fd449289298a8261f0e6fc7d9a78ca15
ZhuangKang <k13659082460@gmail.com>
2025-07-11 22:36:09 +0800

    answer Liu questions regarding the env step function

--- envs/envs.py
+++ envs/envs.py
@@ -1,4 +1,3 @@
-
 import gymnasium as gym
 
 from data_generation.simulation import SimulationUtils, SimulationConfig
@@ -8,57 +7,66 @@ from data_generation.CourierScheduler import CourierScheduler
 import numpy as np
 
 
-
 class DynamicQVRPEnv(gym.Env):
     def __init__(self, config: SimulationConfig):
-        
+
         self.config = config
         # Initialize order generator to create orders with placement times and locations
         self.order_generator = OrderGenerator(config)
         # Initialize courier scheduler to manage base and on-demand couriers
         self.courier_scheduler = CourierScheduler(config)
-        
+
         self.state_manager = StateManager(config)
-        
+
         self.utils = SimulationUtils()
-        
+
         self.decision_epochs = np.arange(
             0, config.H0 + config.decision_interval, config.decision_interval
         )
-        
-    
+
     def reset(self):
-        
+
         self.active_couriers = self.courier_scheduler.base_schedule[:]
-        
+
         self.active_orders = [
             (t, r, d, loc, None) for t, r, d, loc in self.order_generator.get_orders()
         ]
-        
-    
-    
+
     def step(self, t, action):
         a1, a1_5 = action
-        new_couriers = [1]*a1 + [1.5]*a1_5
+        new_couriers = [1] * a1 + [1.5] * a1_5
         t_next = min(t + self.config.decision_interval, self.config.H0)
-        
+
         # 改变骑手
         self.courier_scheduler.add_on_demand_couriers(t, action)
         # Add new couriers to active list: start = t + delta, end = t + delta + c * 60
         self.active_couriers = self.courier_scheduler.get_active_couriers(t)
-        # 计算奖励 
+        # 计算奖励
         n_lost = self.utils.get_lost_orders(
-                t, t_next, self.active_orders, self.active_couriers, self.config
-            )
-        reward = self.config.K_lost * n_lost + sum(self.config.K_c[c] for c in new_couriers)
+            t, t_next, self.active_orders, self.active_couriers, self.config
+        )
+        reward = self.config.K_lost * n_lost + sum(
+            self.config.K_c[c] for c in new_couriers
+        )
         # 改变订单状态
         self.active_orders, _ = self.utils.assign_orders(
             t, self.active_orders, self.active_couriers, self.config
         )
         # 此处是否多余
+        # Kang's answer: It is not redundant as the system won't place new
+        # order's after H_0.
         t_next = min(t + self.config.decision_interval, self.config.H0)
-        
+
         # 此处不理解，这个是临时骑手表，以便计算状态吗？
+        # Kang's Answer:
+        # If I understood correctly, you have adapted this function from the
+        # run_episode function in simulator.
+
+        # However, I have updated the simulator which removed the redundant terms
+        # like the following line.
+
+        # Please find my newest run_episode funcion.
+
         active_couriers = [
             (start, end) for start, end in self.active_couriers if start <= t_next < end
         ]

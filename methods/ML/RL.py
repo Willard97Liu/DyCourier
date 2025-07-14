420590fb50eba79a968b74771db72ede6259a94c
W. Liu <liuw2@orichalcum.liacs.nl>
2025-07-14 21:15:24 +0200

    Initial commit

--- methods/ML/RL.py
+++ methods/ML/RL.py
@@ -116,12 +116,19 @@ class DQN(nn.Module):
 def train_DQN(
     env,
     LR = 1e-4,
-    BATCH_SIZE = 1,
+    BATCH_SIZE = 64,
     hidden_layers = [1024, 1024, 1024],
     EPISODES = 20,
     update_target_every = 100,
     TAU = 0.01,
+    GAMMA = 0.99
 ):
+    
+    device = torch.device(
+        # "cuda" if torch.cuda.is_available() else
+        # "mps" if torch.backends.mps.is_available() else
+        "cpu"
+    )
     num_episodes = EPISODES
     len_state = 7
     ACTIONS = [
@@ -151,73 +158,65 @@ def train_DQN(
         else:
             state = state.float()
             
-        state = state.unsqueeze(0)  
-        action_id = policy_net(state).max(1).indices.view(1, 1)
+        state = state.unsqueeze(0) 
+        action_id = policy_net(state).squeeze(1).max(1).indices.view(1, 1)
         return action_id
     
+    # def optimize_model():
+    #     if len(memory) < BATCH_SIZE:
+    #         return
+    #     transitions = memory.sample(BATCH_SIZE)
+    #     batch = Transition(*zip(*transitions))
+    #     optimizer.zero_grad()
+    #     torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    #     optimizer.step()
+
+
     def optimize_model():
         if len(memory) < BATCH_SIZE:
             return
+        
         transitions = memory.sample(BATCH_SIZE)
         batch = Transition(*zip(*transitions))
+        
+        next_states = torch.cat([s for s in batch.next_state
+                                                    if s is not None])
+
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+        
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+        next_state_values = torch.zeros(BATCH_SIZE, device=device)
+        
+        with torch.no_grad():
+            next_state_values = target_net(next_states).max(1).values
+        
+        next_states = torch.cat([s for s in batch.next_state])
+        
+        expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+        
+        # Compute L2 loss
+        criterion = nn.MSELoss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+        
+        # Optimize the model
         optimizer.zero_grad()
+        
+        loss.backward()
         torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
         optimizer.step()
-
-
-    def optimize_model():
-        if len(memory) < BATCH_SIZE:
-            return
-        # transitions = memory.sample(BATCH_SIZE)
-        # # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-        # # detailed explanation). This converts batch-array of Transitions
-        # # to Transition of batch-arrays.
-        # batch = Transition(*zip(*transitions))
-
-        # # Compute a mask of non-final states and concatenate the batch elements
-        # # (a final state would've been the one after which simulation ended)
-        # non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-        #                                       batch.next_state)), device=device, dtype=torch.bool)
-        # non_final_next_states = torch.cat([s for s in batch.next_state
-        #                                             if s is not None])
-        # state_batch = torch.cat(batch.state)
-        # action_batch = torch.cat(batch.action)
-        # reward_batch = torch.cat(batch.reward)
-
-        # # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-        # # columns of actions taken. These are the actions which would've been taken
-        # # for each batch state according to policy_net
-        # state_action_values = policy_net(state_batch).gather(1, action_batch)
-
-        # # Compute V(s_{t+1}) for all next states.
-        # # Expected values of actions for non_final_next_states are computed based
-        # # on the "older" target_net; selecting their best reward with max(1).values
-        # # This is merged based on the mask, such that we'll have either the expected
-        # # state value or 0 in case the state was final.
-        # next_state_values = torch.zeros(BATCH_SIZE, device=device)
-        # with torch.no_grad():
-        #     next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
-        # # Compute the expected Q values
-        # expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-        # # Compute Huber loss
-        # # criterion = nn.SmoothL1Loss()
-
-        # # Compute L2 loss
-        # criterion = nn.MSELoss()
-        # loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
-
-        # # Optimize the model
-        # optimizer.zero_grad()
-        # loss.backward()
-        # # In-place gradient clipping
-        # torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
-        # optimizer.step()
         
         
     for i_episode in range(num_episodes):
         
         state = env.reset()
+        
+        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
         
         for t in env.decision_epochs:
             
@@ -225,11 +224,19 @@ def train_DQN(
             
             action = ACTIONS[action_id]
             
+            
             reward, next_state = env.step(t, action)
-
-            memory.push(state, action, next_state, reward)
+            
+            next_state = torch.tensor(next_state, dtype=torch.float32, device=device).unsqueeze(0)
+            
+            reward = np.array(reward, np.float32)
+            
+            reward = torch.tensor(reward[None])
+            
+            memory.push(state, action_id, next_state, reward)
             # # Move to the next state
             state = next_state
+            
             # # Perform one step of the optimization (on the policy network)
             optimize_model()
             
@@ -247,6 +254,7 @@ def train_DQN(
                 for key in policy_net_state_dict:
                     target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
                 target_net.load_state_dict(target_net_state_dict)
+                
 
     return 
     
